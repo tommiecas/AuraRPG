ba5bc9014275b3e01f983aeb3d2f7c823278a7f7
Tom Casiello <tomcasiello@gmail.com>
2024-03-02 19:38:14 -0800

    Aura User Widget and Widget Controller

---
+++ Source/Aura/Private/UI/Widgets/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright Crapiello Entertainment
+
+
+#include "UI/Widgets/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
