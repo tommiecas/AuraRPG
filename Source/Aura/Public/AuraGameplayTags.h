bd72d761b99253cef8229ea5d9e45c3612183bf1
Tom Casiello <tomcasiello@gmail.com>
2024-03-09 01:29:09 -0800

    Gameplay Tags Singleton

---
+++ Source/Aura/Public/AuraGameplayTags.h
@@ -0,0 +1,23 @@
+// Copyright Crapiello Entertainment
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+
+/** Aura Gameplay Tags
+ * 
+ * Singleton Containing native Gameplay Tags
+ *
+ */
+
+struct FAuraGameplayTags
+{
+public:
+ static const FAuraGameplayTags& Get() { return GameplayTags;}
+ static void InitializeNativeGameplayTags();
+protected:
+
+private:
+ static FAuraGameplayTags GameplayTags;
+};
