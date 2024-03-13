7317ac2e43a6f82fe9b2e9809125ad7a9381ddde
Tom Casiello <tomcasiello@gmail.com>
2024-03-12 18:06:44 -0700

    Attribute Menu Widget Controller

---
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright Crapiello Entertainment
+
+
+#include "UI/WidgetController/AttributeMenuWidgetController.h"
+
+void UAttributeMenuWidgetController::BindCallbacksToDependencies()
+{
+
+}
+
+void UAttributeMenuWidgetController::BroadcastInitialValues()
+{
+
+}
